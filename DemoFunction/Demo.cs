3b0a1cf5cfc7e86f14e83e2345b0aa323b103615
Carlos Hernandez <caradrherjr@hotmail.com>
2024-12-19 14:10:57 -0800

    Add Function level security

--- DemoFunction/Demo.cs
+++ DemoFunction/Demo.cs
@@ -16,9 +16,10 @@ namespace DemoFunction
         }
 
         [Function("Demo")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            await response.WriteStringAsync("Hello World!");
             return response;
         }
     }
